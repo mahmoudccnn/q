75413a77a373d944b0e58719d6249ccf22070bbf
mahmoudccnn <97877461+mahmoudccnn@users.noreply.github.com>
2023-05-21 08:13:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const navLinksLi = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+  // Toggle nav
+  navLinks.classList.toggle('nav-active');
+
+  // Animate links
+  navLinksLi.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+  });
+
+  // Burger animation
+  burger.classList.toggle('toggle');
+});
